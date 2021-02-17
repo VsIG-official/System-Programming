3573be6ab838c3bbd8f91286f8fd248bc15db6fe
VsIG <Valentyn2202@gmail.com>
2021-02-17 22:56:39 +0200

    ✨: start of asm project

---
+++ Labs/Lab1/Lab1-9-IP-93-Dominskyi.asm
@@ -0,0 +1,18 @@
+.486
+.model flat, stdcall
+
+include\masm32\include\windows.inc
+include\masm32\include\kernel32.inc
+include\masm32\include\user32.inc
+includelib\masm32\lib\kernel32.lib
+includelib\masm32\lib\user32.lib
+
+.data
+
+
+
+.code
+
+main
+
+end main
