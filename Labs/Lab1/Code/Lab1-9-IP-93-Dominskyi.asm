3c9dc39a80ecd559f3e2437dbb6d5c59b49a02e5
VsIG <Valentyn2202@gmail.com>
2021-02-18 22:54:33 +0200

    ✨: some try to add new variables

--- Labs/Lab1/Code/Lab1-9-IP-93-Dominskyi.asm
+++ Labs/Lab1/Code/Lab1-9-IP-93-Dominskyi.asm
@@ -10,19 +10,22 @@ includelib/masm32/lib/user32.lib
 
 .data
 	MsgBoxName  db "Some window with numbers", 0 
-	MsgBoxText     db "A Positive = %s, A Negative = %s", 0
+	MsgBoxText db 
+	MsgBoxTextPosA    db "A Positive = %s", 0
+	MsgBoxTextNegA    db "A Negative = %s", 0
 	
 	APositiveByte db +22, 0
 	ANegativeByte db -22, 0
+	
 	APositiveByteBuffer db 64 DUP(?)
 	ANegativeByteBuffer db 64 DUP(?)
 .code
 
 	Main:
-		invoke wsprintf, addr APositiveByteBuffer, addr MsgBoxText, addr APositiveByte
+		invoke wsprintf, addr APositiveByteBuffer, addr MsgBoxTextPosA, addr APositiveByte
 
-		invoke wsprintf, addr ANegativeByteBuffer, addr MsgBoxText, addr ANegativeByte
+		invoke wsprintf, addr ANegativeByteBuffer, addr MsgBoxTextNegA, addr ANegativeByte
 	
-		invoke MessageBoxA, NULL, addr MsgBoxText, addr MsgBoxName, MB_OK
+		invoke MessageBoxA, NULL, addr MsgBoxTextPosA, addr MsgBoxName, MB_OK
 		invoke ExitProcess, 0
 	end Main
