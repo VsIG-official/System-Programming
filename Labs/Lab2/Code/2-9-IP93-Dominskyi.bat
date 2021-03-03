e9077171cb2583db6ef94e415cfda009555b1556
VsIG <Valentyn2202@gmail.com>
2021-03-03 14:00:45 +0200

    ✨: add some code

--- Labs/Lab2/Code/2-9-IP93-Dominskyi.bat
+++ Labs/Lab2/Code/2-9-IP93-Dominskyi.bat
@@ -1,10 +1,9 @@
-@echo off
-
 set startFolder=%cd%
 set masm_path=D:\masm32\bin
 set dos_box="D:\masm32\DosBox\DOSBox-0.74-3\DOSBox.exe"
 set filename=%1
 
+:: line beneath should be deleted
 cd C:\
 for /f "tokens=* USEBACKQ" %%i in (`dir %filename% /s /b`) do (
 	set asmfile=%%i
@@ -13,12 +12,11 @@ for /f "tokens=* USEBACKQ" %%i in (`dir %filename% /s /b`) do (
 :	next1
 
 cd "%asmfile%\..\"
+:: line beneath should be deleted
 set folder=%cd%
 
-@echo "ASMFILE = %asmfile%"
-@echo "DIR = %folder%"
-
 %masm_path%\ml /Bl %masm_path%\link16.exe %asmfile%
+:: line beneath should be deleted
 %dos_box% -c "mount c %folder% " -c c:
 
 cd %startFolder%
