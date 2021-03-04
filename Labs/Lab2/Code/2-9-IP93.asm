8e19276ef2e70d6743598e76ba504dd8538b1a98
VsIG <valentyn2202@gmail.com>
2021-03-04 23:06:54 +0200

    ✨: add some working code

--- Labs/Lab2/Code/2-9-IP93.asm
+++ Labs/Lab2/Code/2-9-IP93.asm
@@ -1,16 +1,45 @@
+; Processors
 .model tiny
 .386
- 
-.data
-pass1 db '12345'
-len1 = $-pass1
-pass2 db 10,0,10 dup(?)
-msg1  db 0Dh,0Ah,'true$'
-msg2  db 0Dh,0Ah,'false$'
- 
+
+.data?
+
+; Data Segment
+.data	
+	StartingText DB "Введiть пароль. Попереджаю, що у Вас є лише 3 спроби: $"
+	FailureText DB "Пароль невiрний. $"
+	
+	StringFromUser DB 128 DUP (?)
+	StringFromUser2 db 10,0,10 dup(?)
+	
+	; We can write password in two ways:
+	Password  DB '123'
+	
+	; And another one is:
+	; Password  DB 31h 32h 33h
+	
+	PasswordCount = $-Password
+	;MaxLengthOfUsersString DB 128
+	
+	; Text To Show
+	InformationText DB "ПIБ = Домiнський Валентин Олексiйович", 10, 
+		"Дата Народження = 22.02.2002", 10,
+		"Номер Залiковки книжки = 9311 $", 0
+	
+; Code Segment
 .code
-org	100h
-start: 
+	org	100h ; this is offset for com programs:
+	; It defines where the machine code (translated 
+	; assembly program) is to place in memory.
+	; As for org 100h this deals with 80x86 COM 
+	; program format (COMMAND) which consist 
+	; of only one segment of max. 64k bytes. 
+	; 100h says that the machine code starts from 
+	; address (offset) 100h in this segment.
+	; For com format the offset is always 100h
+	
+.startup ; Generates program start-up code
+	InvitePoint:	; Starting Code
 		; Clear the screen
 		mov ax, 0600h
 		mov bh, 7h
@@ -25,45 +54,74 @@ start:
 		mov dh, 00
 		int 10h
 
-		mov ah, 03Fh ; Function to read the file
-		mov cx, 128 ; MaxLengthOfUsersString
-		mov 	dx, offset pass2
-		int 	21h
+		; Display The Text
+		mov dx, offset StartingText
+		mov ah, 9h
+		int 21h
+		
+		jmp InputOfTheUser
+
+	; Responsible For Input
+	InputOfTheUser:
+    mov ah,0Ah
+    lea dx,StringFromUser2
+    int 21h
+
+		mov al,PasswordCount
+		cmp al,[StringFromUser2+1];2
+        jne WrongPasswordByUser
+
+		cld
+		lea si,Password
+		lea di,StringFromUser2+2;2
+		xor cx,cx
+		mov cl,PasswordCount
+		check:
+			lodsb
+			cmp al,byte ptr [di]
+			je step
  
-    mov al,len1
-    cmp al,[pass2+1]
-    jne wrong
- 
-    cld
-    lea si,pass1
-    lea di,pass2+2
-    xor cx,cx
-    mov cl,len1
-    check:
-        lodsb
-        cmp al,byte ptr [di]
-        je step
- 
-        jmp wrong
+			jmp WrongPasswordByUser
         
-        step:
-        inc di
+			step:
+			inc di
     loop check
  
-    good:
-    mov ah,09h
-    lea dx,msg1
-    int 21h
-    
-    jmp exit    
- 
-    wrong:
-    mov ah,09h
-    lea dx,msg2
-    int 21h
- 
-    exit:
-    mov ah,4Ch
-    mov al,0
-    int 21h
-end start
+	; Responsible For Correct Input
+	CorrectPasswordByUser:
+		; Clear the screen
+		mov ax, 0600h
+		mov bh, 7h
+		mov cx, 0000
+		mov dx, 184fh
+		int 10h
+
+		; Set the position of cursor
+		mov ah, 02
+		mov bh, 00
+		mov dl, 00
+		mov dh, 00
+		int 10h
+	
+		mov ah,09h
+		lea dx, InformationText
+		int 21h
+	
+		jmp ExitCode
+	
+	; Responsible For Wrong Input
+	WrongPasswordByUser:
+		mov ah,09h
+		lea dx, FailureText
+		int 21h
+		
+	; Responsible For Exit
+	ExitCode:
+		; For exiting program We can use this code or...
+		;mov ah, 4Ch
+		;mov al, 00h
+		;int 21h
+		
+		; ... this one
+		.exit
+end
