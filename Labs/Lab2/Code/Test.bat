169b559ac495b87a40aec05d94d9248930f8e980
VsIG <valentyn2202@gmail.com>
2021-03-05 07:34:34 +0200

    Update Test.bat

--- Labs/Lab2/Code/Test.bat
+++ Labs/Lab2/Code/Test.bat
@@ -8,4 +8,7 @@ set NameOfTheFileAsParametr=%1
 start D:\masm32\bin\ml /Bl D:\masm32\bin\link16.exe %NameOfTheFileAsParametr%.asm
 :: I'm using DOSBox for emulating DOS. That's why I launch this program there
 :: Launching COM File
+:: mount c %cd% - connect physical folders and drives to virtual drives inside DOSBox
+:: "keyb none 866" - to connect the appropriate code page
+:: %NameOfTheFileAsParametr%.com - launch *.com file
 start D:\masm32\DosBox\DOSBox-0.74-3\DOSBox.exe -c "mount c %cd% " -c c: -c "keyb none 866" -c %NameOfTheFileAsParametr%.com
