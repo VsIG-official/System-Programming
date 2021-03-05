00c1272419a74fd18eaaac6238ca5f29d488f7b6
VsIG <valentyn2202@gmail.com>
2021-03-05 22:02:21 +0200

    ✨: add some code

--- Labs/Lab2/Code/2-9-IP93-Dominskyi.bat
+++ Labs/Lab2/Code/2-9-IP93-Dominskyi.bat
@@ -15,4 +15,4 @@ start D:\masm32\bin\ml /Bl D:\masm32\bin\link16.exe %NameOfTheFileAsParametr%
 :: mount c %cd% - connect physical folders and drives to virtual drives inside DOSBox
 :: "keyb none 866" - to connect the appropriate language
 :: %NameOfTheFileAsParametr:~0,-4%.com - launch %1.com file
-start D:\masm32\DosBox\DOSBox-0.74-3\DOSBox.exe -c "mount c %PathToTheFile:~0,-11% " -c c: -c "keyb none 866" -c "%NameOfTheFileAsParametr:~0,-4%.com"
+start D:\masm32\DosBox\DOSBox-0.74-3\DOSBox.exe -c "mount c %PathToTheFile:~0,-26% " -c c: -c "keyb none 866" -c "%NameOfTheFileAsParametr:~0,-4%.com"
