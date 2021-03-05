f2925c6ecaad026668c59046aa64f231b7ad1e38
VsIG <valentyn2202@gmail.com>
2021-03-05 21:30:26 +0200

    ✨: add some code

--- Labs/Lab2/Test.bat
+++ Labs/Lab2/Test.bat
@@ -1,7 +1,13 @@
-:: Creating variable
+:: Creating variables
+:: Name of the file to search
 set NameOfTheFileAsParametr=%1
+:: Name of the Disk
+set NameOfDisk=%2
 
-for /r \ %%a in (%NameOfTheFileAsParametr%) do set "location=%%~dpa"
+:: Search for file
+for /f "tokens=*" %%a in ('dir %NameOfTheFileAsParametr% /b /s') do set PathToTheFile=%%a
+
+cd "%PathToTheFile%\..\"
 
 :: Creating COM File
 start D:\masm32\bin\ml /Bl D:\masm32\bin\link16.exe %NameOfTheFileAsParametr%
@@ -11,4 +17,4 @@ start D:\masm32\bin\ml /Bl D:\masm32\bin\link16.exe %NameOfTheFileAsParametr%
 :: mount c %cd% - connect physical folders and drives to virtual drives inside DOSBox
 :: "keyb none 866" - to connect the appropriate language
 :: %NameOfTheFileAsParametr:~0,-4%.com - launch %1.com file
-start D:\masm32\DosBox\DOSBox-0.74-3\DOSBox.exe -c "mount c %cd% " -c c: -c "keyb none 866" -c "%NameOfTheFileAsParametr:~0,-4%.com"
+start D:\masm32\DosBox\DOSBox-0.74-3\DOSBox.exe -c "mount c %PathToTheFile:~0,-11% " -c c: -c "keyb none 866" -c "%NameOfTheFileAsParametr:~0,-4%.com"
