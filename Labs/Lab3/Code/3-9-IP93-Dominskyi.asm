4ebaff2415639623c218c413f659743d12c9cc8c
VsIG <valentyn2202@gmail.com>
2021-03-19 00:06:00 +0200

    ✨: start better adaptation

--- Labs/Lab3/Code/3-9-IP93-Dominskyi.asm
+++ Labs/Lab3/Code/3-9-IP93-Dominskyi.asm
@@ -3,21 +3,40 @@
 .model flat, stdcall
 option  CaseMap:None
 
-WinMainProto proto :dword,:dword,:dword,:dword
-CheckPasswordProto   proto :dword,:dword,:dword,:dword
-GetTextDialogProto PROTO :DWORD,:DWORD,:DWORD
+; WinMainProto proto :dword,:dword,:dword,:dword
+; CheckPasswordProto   proto :dword,:dword,:dword,:dword
+; GetTextDialogProto PROTO :DWORD,:DWORD,:DWORD
 
 ; Libraries And Macroses
     include \masm32\include\masm32rt.inc
     include \masm32\include\dialogs.inc
 
-.data?
-	hInstance HINSTANCE ?        ; Handle of our program
+; .data?
+	; hInstance HINSTANCE ?        ; Handle of our program
 	
-	StringFromUser DB 128 dup(?)
+	; StringFromUser DB 128 dup(?)
 
-; Data Segment
-.data	
+; ; Data Segment
+
+
+; .const
+; IDC_EDIT EQU 1001
+; IDC_TEXT EQU 1002
+    ; msg_title  EQU "Лаба 3"
+	   ; msg_pass   EQU "Введіть пароль, будь ласка:"
+
+IDC_EDIT EQU 1001
+IDC_TEXT EQU 1002
+
+MainDlgProc PROTO :DWORD, :DWORD, :DWORD, :DWORD
+ErrorDlgProc PROTO :DWORD, :DWORD, :DWORD, :DWORD
+DataDlgProc PROTO :DWORD, :DWORD, :DWORD, :DWORD
+
+.data?
+    hInstance   DD ?
+    usrInput    DB 64 DUP (?)
+
+.data
 	StartingText DB "Введiть пароль. Попереджаю, що у Вас є лише 4 спроби:  ", 0
 	AdditionalText DB "Пароль вводити сюди",0
 	FailureText DB "Пароль невiрний. Спробуйте ще раз. К-сть спроб, яка залишилася =  ",  0
@@ -40,26 +59,27 @@ GetTextDialogProto PROTO :DWORD,:DWORD,:DWORD
 		 
 	NameOfTheStartingWindows DB "Window with starting text",0        ; the name of our window class
 
-.const
-IDC_EDIT EQU 1001
-IDC_TEXT EQU 1002
     msg_title  EQU "Лаба 3"
-	   msg_pass   EQU "Введіть пароль, будь ласка:"
+    msg_pass   EQU "Введіть пароль, будь ласка:"
+    msg_data   EQU 10, "ОСОБИСТІ ДАНІ:", 10 ,\
+        "ПІБ - КОВАЛИШИН О. Ю.", 10,      \
+        "ДАТА НАРОДЖЕННЯ - 12.09.2001", 10,\
+        "НОМЕР ЗАЛІКОВКИ - 8410", 0
+    msg_error  EQU 10, "Неправильний пароль, спробуйте ще раз!", 0
+    password   DB "bsd~}d"
+    passKey	   DB 12h
+    passLen    DWORD 6
 
 ; Code Segment
 .code
 start: ; Generates program start-up code
-	InvitePoint:	; Starting Code
-
 	invoke MessageBox, 0, offset StartingText, offset MsgBoxName, MB_OK
-
-    mov hInstance, rv(GetModuleHandle,NULL)
-    call WinMain
 	
-	invoke ExitProcess, eax ; quit program. code returns in EAX register from Main Function.
+        MOV hInstance, FUNC(GetModuleHandle, NULL)
+        CALL mainWindow
+        INVOKE ExitProcess, 0
 
-	WinMain proc
-
+    mainWindow PROC
         Dialog msg_title, "Monotype Corsiva", 20,    \
             WS_OVERLAPPED or WS_SYSMENU or DS_CENTER, \
             3,                                         \
@@ -70,19 +90,50 @@ start: ; Generates program start-up code
         DlgEdit WS_BORDER or ES_WANTRETURN, 3, 20, 140, 9, IDC_EDIT
         DlgButton "OK", WS_TABSTOP, 50, 35, 50, 15, IDOK
         
-        CallModalDialog hInstance, 0, WinMainProto, NULL
+        CallModalDialog hInstance, 0, MainDlgProc, NULL
         RET
+    mainWindow ENDP
 
-	WinMain endp
-
-WinMainProto PROC hWin:DWORD, uMsg:DWORD, wParam:DWORD, lParam:DWORD
+    MainDlgProc PROC hWin:DWORD, uMsg:DWORD, wParam:DWORD, lParam:DWORD
         LOCAL count:DWORD
         .IF uMsg == WM_COMMAND
             .IF wParam == IDOK
-                INVOKE EndDialog, hWin, 0
+                MOV count, FUNC(GetDlgItemText, hWin, IDC_EDIT, ADDR usrInput, 512)
+                MOV EAX, passLen
+                .IF count != EAX
+                    JMP error
                 .ENDIF
-.ENDIF
+
+                MOV EDI, 0
+                validation:
+                MOV DL, password[EDI]
+                MOV DH, usrInput[EDI]
+                XOR DH, passKey
+                .IF DL != DH
+                    JMP error
+                .ENDIF
+
+                INC EDI
+                .IF EDI == count
+                    JMP success
+                .ENDIF
+
+                JMP validation
+
+                success:
+                    invoke MessageBox, 0, offset InformationText, offset MsgBoxName, MB_OK
+                    RET
+
+                error:
+                    	invoke MessageBox, 0, offset FailureText, offset MsgBoxName, MB_OK
+                    RET
+            .ENDIF
+        .ELSEIF uMsg == WM_CLOSE
+            INVOKE EndDialog, hWin, 0
+        .ENDIF
+
+        XOR EAX, EAX
         RET
-    WinMainProto ENDP
+    MainDlgProc ENDP
 
 end start
