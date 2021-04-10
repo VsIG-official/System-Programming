3429d212522399ec997df27b7e2acb4a5802a800
VsIG <valentyn2202@gmail.com>
2021-04-10 17:27:13 +0300

    ✨: add something

--- Labs/Lab5/Code/5-9-IP93-Dominskyi.asm
+++ Labs/Lab5/Code/5-9-IP93-Dominskyi.asm
@@ -3,8 +3,8 @@
 .model flat, stdcall
 option CaseMap:None
 
-WinMainProto proto :dword,:dword,:dword
 WinWarningProto proto :dword,:dword,:dword
+WinFailureProto proto :dword,:dword,:dword
 
 ; Libraries And Macroses
     include \masm32\include\windows.inc
@@ -33,16 +33,21 @@ endm
 
 .data?
 	hInstance HINSTANCE ? ; Handle of our program
-	hWndOfMainWindow HWND ? ; Handle of our main window
 	hWndOfWarnWindow HWND ? ; Handle of our warn window
+	hWndOfFailureWindow HWND ? ; Handle of our failure window
+	
+	StringFromUser DB 128 dup(?)
 
 ; Data Segment
 .data
+	StartingText DB "У наступному вікні Ви побачите 5 різних арифметичних виразів", 0
+	FailureText DB "Пароль невiрний. Спробуйте ще раз",  0
+	
 	; Name Of Message Box
 	MsgBoxName  DB "5-9-IP93-Dominskyi", 0
 
-	NameOfTheStartingWindows DB "Window with starting text", 0 ; the name of our window class
 	NameOfTheWarnWindows DB "Window with warn text", 0 ; the name of our success window class
+	NameOfFailureWindows DB "Window with failure text", 0 ; the name of our success window class
 	
 	NameOfTheButton DB "Button", 0 ; the name of our button class
 	NameOfTheText DB "Static", 0 ; the name of our text class
@@ -53,9 +58,11 @@ endm
 .code
 start: ; Generates program start-up code
 	invoke WinWarningProto, hInstance,NULL, SW_SHOWDEFAULT ;invoke function
-	;PrintInformationInWindow 16, 
+
 	invoke GetModuleHandle, NULL
 	mov hInstance, eax
+
+	invoke WinFailureProto, hInstance,NULL, SW_SHOWDEFAULT ;invoke function
 
 	invoke ExitProcess, eax ; quit program. code returns in EAX register from Main Function.
 
@@ -90,7 +97,7 @@ start: ; Generates program start-up code
                 offset NameOfTheWarnWindows,
                 offset MsgBoxName,
                 WS_OVERLAPPEDWINDOW or DS_CENTER,
-                420, 310, 300, 150,
+                520, 310, 200, 150,
                 NULL, NULL, hInst, NULL
 		mov hWndOfWarnWindow, eax
 
@@ -124,6 +131,104 @@ start: ; Generates program start-up code
 	;and has the same name as in the PROC directive
 WinWarningProto endp
 
+; function declaration of WinSuccess
+WinFailureProto  proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdShow:dword
+	; there we need LOCAL variables
+	LOCAL wc:WNDCLASSEX
+    LOCAL msg:MSG
+    LOCAL hwnd:HWND
+
+	; assign variables of WNDCLASSEX
+	; window class is a specification of a window
+    mov   wc.cbSize, sizeof WNDCLASSEX
+    mov   wc.style, CS_HREDRAW or CS_VREDRAW
+    mov   wc.lpfnWndProc, offset WndFailureProc
+    mov   wc.cbClsExtra, NULL
+    mov   wc.cbWndExtra, NULL
+    push  hInstance
+    pop   wc.hInstance
+    mov   wc.hbrBackground, COLOR_WINDOW+1
+    mov   wc.lpszMenuName, NULL
+    mov   wc.lpszClassName, offset NameOfFailureWindows
+    invoke LoadIcon, NULL, IDI_APPLICATION
+    mov   wc.hIcon, eax
+    mov   wc.hIconSm, eax
+    invoke LoadCursor, NULL, IDC_ARROW
+    mov   wc.hCursor, eax
+	
+	; create class of the window
+    invoke RegisterClassEx, addr wc
+    invoke CreateWindowEx, NULL,
+                offset NameOfFailureWindows,
+                offset MsgBoxName,
+                WS_OVERLAPPEDWINDOW or DS_CENTER,
+                510, 280, 220, 150,
+                NULL, NULL, hInst, NULL
+		mov hWndOfFailureWindow, eax
+
+	; write window handle in eax
+    mov   hwnd,eax
+	
+	; Show window
+    invoke ShowWindow, hwnd,CmdShow
+	; update screen
+    invoke UpdateWindow, hwnd
+
+	; waits for message
+    .while TRUE
+				;returns FALSE IF WM_QUIT message is received and will kill the loop
+                invoke GetMessage, addr msg,NULL,0,0
+                .break .IF (!eax)
+				;takes raw keyboard input and generates a new message
+                invoke TranslateMessage, addr msg
+				;sends the message data to the window procedure responsible for the specific window the message is for
+                invoke DispatchMessage, addr msg
+	; end while
+   .endw
+
+   ; code returns in EAX register from Main Function.
+	mov	eax, msg.wParam
+	
+	; return
+	ret
+   
+	;The ENDP directive defines the end of the procedure
+	;and has the same name as in the PROC directive
+WinFailureProto endp
+
+WndFailureProc proc hWnd:HWND, ourMSG:UINT, wParam:WPARAM, lParam:LPARAM
+	; on window close
+	.IF ourMSG==WM_CLOSE
+		; exit program
+		invoke DestroyWindow,hWnd
+        invoke PostQuitMessage,NULL 
+
+    .ELSEIF ourMSG==WM_CREATE
+		; invoke macros #1 one time to create text
+		PrintInformationInWindow 10, offset FailureText
+		; create button
+		invoke CreateWindowEx,NULL,
+                offset NameOfTheButton, offset TextForOKButton,
+                WS_VISIBLE or WS_CHILD or BS_CENTER or BS_TEXT or BS_VCENTER,
+                65, 65, 70, 30,
+                hWnd, 7033, hInstance, NULL
+				
+	.ELSEIF ourMSG==WM_COMMAND
+		; exit program
+		invoke DestroyWindow,hWnd
+        invoke PostQuitMessage,NULL 
+	
+    .ELSE
+		; process the message
+        invoke DefWindowProc,hWnd,ourMSG,wParam,lParam
+        ret
+    .ENDIF
+	
+	ExitCode:
+    xor eax, eax
+    ret
+WndFailureProc endp
+
 WndWarnProc proc hWnd:HWND, ourMSG:UINT, wParam:WPARAM, lParam:LPARAM
 	; on window close
 	.IF ourMSG==WM_CLOSE
@@ -135,20 +240,25 @@ WndWarnProc proc hWnd:HWND, ourMSG:UINT, wParam:WPARAM, lParam:LPARAM
 		invoke CreateWindowEx,NULL,
                 offset NameOfTheButton, offset TextForOKButton,
                 WS_CHILD or WS_VISIBLE or BS_CENTER or BS_TEXT or BS_VCENTER,
-                55, 155, 70, 30,
+                55, 65, 70, 30,
                 hWnd, 7003, hInstance, NULL
-
+		invoke CreateWindowEx,NULL,
+                offset NameOfTheText, offset StartingText,
+                WS_VISIBLE or WS_CHILD or BS_TEXT or SS_CENTER  or BS_VCENTER,
+                16, 10, 150, 50,
+                hWnd, 7004, hInstance, NULL
+				
 	.ELSEIF ourMSG==WM_COMMAND
 		; exit program
 		invoke DestroyWindow,hWnd
         invoke PostQuitMessage,NULL 
-
+	  
     .ELSE
 		 ; process the message
         invoke DefWindowProc,hWnd,ourMSG,wParam,lParam
         ret
     .ENDIF
-
+	 
 	ExitCode:
     xor    eax,eax
     ret
