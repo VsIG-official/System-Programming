6706672969641bb4d492df83130b20d12f6538a7
VsIG-official <valentyn2202@gmail.com>
2021-04-30 20:10:46 +0300

    📝: add a lot of commentaries

--- Labs/Lab6/Code/6-9-IP93-Dominskyi.asm
+++ Labs/Lab6/Code/6-9-IP93-Dominskyi.asm
@@ -41,89 +41,116 @@ DoArithmeticOperations macro aFloat, bFloat, cFloat, dFloat
 	
 	finit ; FPU Initialization
 	
-	fld firstConstant ; st(0) = 2
-	fld cFloat		 ; st(0) = c, st(1) = 2
-	fmul 			 ; st(0) = st(1) * st(0)
+	; 2 * c
+	
+	; move 2 into st(0)
+	fld firstConstant
+	; move c into st(0) and 2 into st(1)
+	fld cFloat
+	; multiply 2 by c and move result into st(0)
+	fmul
 
-	;fstp TwoMulC
+	; convert float to text with 7 digits after "," into buffer
 	invoke FpuFLtoA, 0, 7, addr BufferTwoMulC, SRC1_FPU or SRC2_DIMM
-	; 2*c
-	; ^ works
 	
-	fld dFloat ; st(0) = d, st(1) = 2*c
-	fld secondConstant ; st(0) = 23, st(1) = d, st(2) = 2*c
+	; d / 23
+	
+	; move d into st(0) and 2*c into st(1)
+	fld dFloat
+	; move 23 into st(0), d into st(1) and 2*c into st(2)
+	fld secondConstant
 
-	fdiv ; st(0) = st(1)/st(0) = d/23, st(1) = 2*c
+	; divide d by 23 and move result into st(0), 2*c to st(1)
+	fdiv
 	
-	;fstp DdivTwenThree
+	; convert float to text with 7 digits after "," into buffer
 	invoke FpuFLtoA, 0, 7, addr BufferDdivTwenThree, SRC1_FPU or SRC2_DIMM
-	; d/23
-	; ^ work
 	
-	fsub ; st(0) = st(1) - st(0) = 2*c - d/23
+	; 2 * c - d / 23
 	
-	;fstp FirstPart
+	; subtract d/23 from 2*c, move result into st(0)
+	fsub
+	
+	; convert float to text with 7 digits after "," into buffer
 	invoke FpuFLtoA, 0, 7, addr BufferFirstPart, SRC1_FPU or SRC2_DIMM
-	; 2*c-d/23
-	; ^ works
 	
-	fldln2 ; st(0) = ln(2), st(1) = 2*c-d/23
-	; st(0) = ln(2), st(1) =  ln(b - a/4), st(2) = 2*c-d/23
+	; move ln(2) into st(0) and 2*c-d/23 into st(1)
+	fldln2
 	
-	fld bFloat ; st(0) = b, st(1) = ln(2), st(2) = 2*c-d/23
+	; move b into st(0), ln(2) into st(1) and 2*c-d/23 into st(2)
+	fld bFloat
 	
-	fld aFloat ; st(0) = a, st(1) = b, st(2) = ln(2), st(3) = 2*c-d/23
-	fld thirdConstant ; st(0) = 4, st(1) = a, st(2) = b, st(3) = ln(2), st(4) = 2*c-d/23
+	; move a into st(0), b into st(1), ln(2) into st(2) 2*c-d/23 into st(3)
+	fld aFloat
+	; move 4 into st(0), a into st(1), b into st(2), ln(2) into st(3) and 2*c-d/23 into st(4)
+	fld thirdConstant
 	
-	fdiv ; st(0) = st(1)/st(0) = a/4, st(1) = b, st(2) = ln(2), st(3) = 2*c-d/23
+	; divide a by 4 and move it into st(0), b into st(1), ln(2) into st(2) and 2*c-d/23 into st(3)
+	fdiv
 	
-	;fstp AdivFour
+	; convert float to text with 7 digits after "," into buffer
 	invoke FpuFLtoA, 0, 7, addr BufferAdivFour, SRC1_FPU or SRC2_DIMM
-	; a/4
-	; ^ works
+
+	; subtract a/4 from b, move result into st(0), ln(2) into st(1) and 2*c-d/23 into st(2)
+	fsub
 	
-	fsub ; st(0) = st(1) - st(0) = b - a/4, st(1) = ln(2), st(2) = 2*c-d/23
-	
-	;fstp BsubPartOfLn
+	; convert float to text with 7 digits after "," into buffer
 	invoke FpuFLtoA, 0, 7, addr BufferBsubPartOfLn, SRC1_FPU or SRC2_DIMM
-	; b-a/4
-	; ^ works
 	
-	; zero or less comparing
+	; compare, if number is zero or less for ln
+	
+	; compares the contents of st (0) to the source
 	fcom zero
+	; saves the current value of the SR register to the receiver
 	fstsw ax
+	; loads flags
 	sahf
+	; jump, if equal to zero
 	je NumberIsLessOrZero
+	; jump, if less than zero
 	jb NumberIsLessOrZero
 	
-	fyl2x ; st(0) = st(1)(st(0)) = ln(b - a/4), st(1) = 2*c-d/23
+	; find ln(b - a/4) and move it into st(0), 2*c-d/23 into st(1)
+	fyl2x
 	
-	;fstp SecondPart
+	; convert float to text with 7 digits after "," into buffer
 	invoke FpuFLtoA, 0, 7, addr BufferSecondPart, SRC1_FPU or SRC2_DIMM
-	; ln(b-a/4)
-	; ^ works
 	
-	; zero comparing
+	; compare, if number is zero for dividing
+	
+	; compares the contents of st (0) to the source
 	fcom zero
+	; saves the current value of the SR register to the receiver
 	fstsw ax
+	; loads flags
 	sahf
+	; jump, if equal to zero.zero
 	je NumberIsZero
 	
+	; compares the contents of st (0) to the source
 	fcom negativeZero
+	; saves the current value of the SR register to the receiver
 	fstsw ax
+	; loads flags
 	sahf
+	; jump, if equal to negative zero.zero
 	je NumberIsZero
 	
+	; compares the contents of st (0) to zero
 	ftst
+	; saves the current value of the SR register to the receiver
 	fstsw ax
+	; loads flags
 	sahf
+	; jump, if equal to zero
 	je NumberIsZero
 	
-	fdiv ; st(0) = st(1)/st(0) = (2*c-d/23)/(ln(b-a/4))
+	; divides 2*c-d/23 by ln(b-a/4) and move it into st(0)
+	fdiv
 	
 	; (2 * c - d / 23) / (ln( b - a / 4))
-	; ^ works
 	
+	; saves st(0) into variable
 	fstp floatFinal
 
 	;; value for final result
@@ -148,7 +175,7 @@ DoArithmeticOperations macro aFloat, bFloat, cFloat, dFloat
 		;; parsing variables into TempPlaceForText
 		invoke wsprintf, addr TempPlaceForText, addr NegativeOrZeroLnText
 		jmp EndThisMacros
-		
+
 	EndThisMacros:
 endm
 
@@ -169,7 +196,7 @@ endm
 	
 	; Start = (2 * c - d / 23) / (ln(b - a / 4))
 	
-	; Values for intermediate results
+	; Buffers for intermediate results
 	
 	; First Step
 	; Value of 2 * c
