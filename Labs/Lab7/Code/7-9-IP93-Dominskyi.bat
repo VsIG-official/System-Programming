fc97dd2005cad246433efc1eb42ac366e5b5be49
VsIG-official <valentyn2202@gmail.com>
2021-05-13 08:49:28 +0300

    🚧: smoll update

--- Labs/Lab7/Code/7-9-IP93-Dominskyi.bat
+++ Labs/Lab7/Code/7-9-IP93-Dominskyi.bat
@@ -1,9 +1,6 @@
 @echo off
     set filename="7-9-IP93-Dominskyi"
     set exec_filename="7-9-IP93-Dominskyi.exe"
-    if exist "%filename%.obj" del "%filename%.obj"
-    if exist "%filename%-1.obj" del "%filename%-1.obj"
-    if exist "%filename%.exe" del "%filename%.exe"
 
     \masm32\bin\ml /c /coff "%filename%.asm"
     \masm32\bin\ml /c /coff "%filename%-1.asm"
