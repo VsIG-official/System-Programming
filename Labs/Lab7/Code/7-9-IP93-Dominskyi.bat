6e03d1306c73648a51278355a15e95069c8ec14f
VsIG-official <valentyn2202@gmail.com>
2021-05-13 22:30:40 +0300

    ✨: create better [.bat]

--- Labs/Lab7/Code/7-9-IP93-Dominskyi.bat
+++ Labs/Lab7/Code/7-9-IP93-Dominskyi.bat
@@ -1,27 +1,7 @@
-@echo off
-    set filename="7-9-IP93-Dominskyi"
-    set exec_filename="7-9-IP93-Dominskyi.exe"
+set filename="7-9-IP93-Dominskyi"
+set exec_filename="7-9-IP93-Dominskyi.exe"
 
-    \masm32\bin\ml /c /coff "%filename%.asm"
-    \masm32\bin\ml /c /coff "%filename%-1.asm"
-    if errorlevel 1 goto errasm
-
-    \masm32\bin\Link.exe /SUBSYSTEM:WINDOWS /out:%exec_filename% "%filename%.obj" "%filename%-1.obj"
-    if errorlevel 1 goto errlink
-    dir "%filename%.*"
-    goto TheEnd
-
-  :errlink
-    echo _
-    echo Link error
-    goto TheEnd
-
-  :errasm
-    echo _
-    echo Assembly Error
-    goto TheEnd
-    
-  :TheEnd
-
-%filename%.exe
-pause
+\masm32\bin\ml /c /coff "%filename%.asm"
+\masm32\bin\ml /c /coff "%filename%-1.asm"
+	
+\masm32\bin\Link.exe /SUBSYSTEM:WINDOWS /out:%exec_filename% "%filename%.obj" "%filename%-1.obj"
