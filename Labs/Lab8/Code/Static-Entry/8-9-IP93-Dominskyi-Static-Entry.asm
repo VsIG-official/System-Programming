cda10daebf9a4ac55085f83591e2180663b89f5e
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-05-28 07:14:46 +0300

    ✨: add static entry

---
+++ Labs/Lab8/Code/Static-Entry/8-9-IP93-Dominskyi-Static-Entry.asm
@@ -0,0 +1,319 @@
+; Processors
+.386
+.model flat, stdcall
+option CaseMap:None
+
+DoArithmeticOperations proto :ptr qword, :ptr qword, :ptr qword, :ptr qword, :dword
+WinWarningProto proto :dword,:dword,:dword
+WinMainProto proto :dword,:dword,:dword
+
+; Libraries And Macroses
+includelib 8-9-IP93-Dominskyi-Static-Entry-Library.lib
+includelib /masm32/lib/Fpu.lib
+include /masm32/include/Fpu.inc
+include /masm32/include/masm32rt.inc
+
+; Our Macroses
+; We place them here, 'cause it won't degrade the readability of the code
+
+; Macros #1 for printing some text
+PrintInformationInWindow macro heightPosition, infoToShow
+	; for example, this commentary is included into macroexpansion
+	;; but this - not
+	
+	; just pass position of the text on vertical
+	; and text, that We want to show
+	invoke CreateWindowEx,NULL,
+            offset NameOfTheText, offset infoToShow,
+            WS_VISIBLE or WS_CHILD or BS_TEXT or SS_CENTER  or BS_VCENTER,
+            16, heightPosition, 890, 75,
+            hWnd, 7044, hInstance, NULL
+endm
+
+.data?
+	hInstance HINSTANCE ? ; Handle of our program
+	hWndOfWarnWindow HWND ? ; Handle of our warn window
+	hWndOfMainWindow HWND ? ; Handle of our main window
+
+; Data Segment
+.data
+	StartingText DB "У наступному вікні Ви побачите 5 різних арифметичних виразів", 13, 0
+	TempPlaceForText DB 1024 DUP(0)
+	; Name Of Message Box
+	MsgBoxName  DB "8-9-IP93-Dominskyi", 0
+
+	NameOfTheWarnWindows DB "Window with warn text", 0 ; the name of our warn window class
+	NameOfMainWindows DB "Window with main text", 0 ; the name of our success window class
+	
+	NameOfTheButton DB "Button", 0 ; the name of our button class
+	NameOfTheText DB "Static", 0 ; the name of our text class
+	
+	TextForOKButton DB "ОК", 0
+	
+	; My equation = (2 * c - d / 23) / (ln( b - a / 4))
+	
+	; can't be 1 or 0
+	; first way of declaring array
+	FloatsA dq 2.0, -16.0, -68.946, 0.001, 4.0 ;; first numbers
+	FloatsB dq 4.0, 23.091, 6.67, -3.33, 2.0 ;; second numbers
+	FloatsC dq -99.0, -2.111, -78.2, 123.4, 44.47 ;; third numbers
+	
+	; and the second one
+	FloatsD dq -15.125 ;; fourth numbers
+			  dq 0.5
+			  dq -22.1
+			  dq -9.0
+			  dq 12.2222
+	
+	; for automating 
+	possibleHeight DD 25
+	coefficientOfMultiplyingForTextHeight DD 3
+
+	; first text to show
+	variantToShow DB "My equation = (2 * c - d / 23) / (ln(b - a / 4))", 13, 0
+
+; Code Segment
+.code
+start: ; Generates program start-up code
+	invoke WinWarningProto, hInstance,NULL, SW_SHOWDEFAULT ;invoke function
+
+	invoke GetModuleHandle, NULL
+	mov hInstance, eax
+
+	invoke WinMainProto, hInstance,NULL, SW_SHOWDEFAULT ;invoke function
+
+	invoke ExitProcess, eax ; quit program. code returns in EAX register from Main Function.
+
+	; function declaration of WinWarn
+	WinWarningProto  proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdShow:dword
+	; there we need LOCAL variables
+	LOCAL wc:WNDCLASSEX
+    LOCAL msg:MSG
+    LOCAL hwnd:HWND
+
+	; assign variables of WNDCLASSEX
+	; window class is a specification of a window
+    mov   wc.cbSize, sizeof WNDCLASSEX
+    mov   wc.style, CS_HREDRAW or CS_VREDRAW
+    mov   wc.lpfnWndProc, offset WndWarnProc
+    mov   wc.cbClsExtra, NULL
+    mov   wc.cbWndExtra, NULL
+    push  hInstance
+    pop   wc.hInstance
+    mov   wc.hbrBackground, COLOR_WINDOW+1
+    mov   wc.lpszMenuName, NULL
+    mov   wc.lpszClassName, offset NameOfTheWarnWindows
+    invoke LoadIcon, NULL, IDI_APPLICATION
+    mov   wc.hIcon, eax
+    mov   wc.hIconSm, eax
+    invoke LoadCursor, NULL, IDC_ARROW
+    mov   wc.hCursor, eax
+
+	; create class of the window
+    invoke RegisterClassEx, addr wc
+    invoke CreateWindowEx, NULL,
+                offset NameOfTheWarnWindows,
+                offset MsgBoxName,
+                WS_OVERLAPPEDWINDOW or DS_CENTER,
+                470, 310, 300, 150,
+                NULL, NULL, hInst, NULL
+		mov hWndOfWarnWindow, eax
+
+	; write window handle in eax
+    mov   hwnd,eax
+	
+	; Show window
+    invoke ShowWindow, hwnd,CmdShow
+	; update screen
+    invoke UpdateWindow, hwnd
+
+	; waits for message
+    .while TRUE
+				;returns FALSE IF WM_QUIT message is received and will kill the loop
+                invoke GetMessage, addr msg,NULL,0,0
+                .break .IF (!eax)
+				;takes raw keyboard input and generates a new message
+                invoke TranslateMessage, addr msg
+				;sends the message data to the window procedure responsible for the specific window the message is for
+                invoke DispatchMessage, addr msg
+	; end while
+   .endw
+
+   ; code returns in EAX register from Main Function.
+	mov	eax, msg.wParam
+
+	; return
+	ret
+
+	;The ENDP directive defines the end of the procedure
+	;and has the same name as in the PROC directive
+WinWarningProto endp
+
+; function declaration of WinSuccess
+WinMainProto  proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdShow:dword
+	; there we need LOCAL variables
+	LOCAL wc:WNDCLASSEX
+    LOCAL msg:MSG
+    LOCAL hwnd:HWND
+
+	; assign variables of WNDCLASSEX
+	; window class is a specification of a window
+    mov   wc.cbSize, sizeof WNDCLASSEX
+    mov   wc.style, CS_HREDRAW or CS_VREDRAW
+    mov   wc.lpfnWndProc, offset WndMainProc
+    mov   wc.cbClsExtra, NULL
+    mov   wc.cbWndExtra, NULL
+    push  hInstance
+    pop   wc.hInstance
+    mov   wc.hbrBackground, COLOR_WINDOW+1
+    mov   wc.lpszMenuName, NULL
+    mov   wc.lpszClassName, offset NameOfMainWindows
+    invoke LoadIcon, NULL, IDI_APPLICATION
+    mov   wc.hIcon, eax
+    mov   wc.hIconSm, eax
+    invoke LoadCursor, NULL, IDC_ARROW
+    mov   wc.hCursor, eax
+	
+	; create class of the window
+    invoke RegisterClassEx, addr wc
+    invoke CreateWindowEx, NULL,
+                offset NameOfMainWindows,
+                offset MsgBoxName,
+                WS_OVERLAPPEDWINDOW or DS_CENTER,
+                170, 50, 940, 550,
+                NULL, NULL, hInst, NULL
+		mov hWndOfMainWindow, eax
+
+	; write window handle in eax
+    mov   hwnd,eax
+	
+	; Show window
+    invoke ShowWindow, hwnd,CmdShow
+	; update screen
+    invoke UpdateWindow, hwnd
+
+	; waits for message
+    .while TRUE
+				;returns FALSE IF WM_QUIT message is received and will kill the loop
+                invoke GetMessage, addr msg,NULL,0,0
+                .break .IF (!eax)
+				;takes raw keyboard input and generates a new message
+                invoke TranslateMessage, addr msg
+				;sends the message data to the window procedure responsible for the specific window the message is for
+                invoke DispatchMessage, addr msg
+	; end while
+   .endw
+
+   ; code returns in EAX register from Main Function.
+	mov	eax, msg.wParam
+	
+	; return
+	ret
+   
+	;The ENDP directive defines the end of the procedure
+	;and has the same name as in the PROC directive
+WinMainProto endp
+
+WndMainProc proc hWnd:HWND, ourMSG:UINT, wParam:WPARAM, lParam:LPARAM
+	; on window close
+	.IF ourMSG==WM_CLOSE
+		; exit program
+		invoke DestroyWindow,hWnd
+        invoke PostQuitMessage,NULL 
+
+    .ELSEIF ourMSG==WM_CREATE
+
+		mov edi, 0
+		; invoke macros #1 one time to create text
+		PrintInformationInWindow possibleHeight, offset variantToShow
+
+		;; do the loop
+		LoopItself:
+		
+		invoke DoArithmeticOperations, addr FloatsA[8*edi],addr FloatsB[8*edi],addr FloatsC[8*edi], addr FloatsD[8*edi], addr TempPlaceForText
+		
+		; mov possibleHeight into eax
+		mov eax, possibleHeight
+		;; Convert byte to word
+		cbw
+		
+		; mov possibleHeight into ebx
+		mov ebx, coefficientOfMultiplyingForTextHeight
+		;; Convert byte to word
+		cbw
+		
+		;; coefficientOfMultiplyingForTextHeight * possibleHeight
+		;; eax * ebx
+		imul ebx
+
+		imul esi
+		
+		; print text
+		PrintInformationInWindow eax, TempPlaceForText
+		
+		inc edi
+		inc esi
+		
+		cmp edi, 5
+		jne LoopItself
+		
+		; create button
+		invoke CreateWindowEx,NULL,
+                offset NameOfTheButton, offset TextForOKButton,
+                WS_VISIBLE or WS_CHILD or BS_CENTER or BS_TEXT or BS_VCENTER,
+                395, 465, 150, 30,
+                hWnd, 7033, hInstance, NULL
+
+	.ELSEIF ourMSG==WM_COMMAND
+		; exit program
+		invoke DestroyWindow,hWnd
+        invoke PostQuitMessage,NULL 
+	
+    .ELSE
+		; process the message
+        invoke DefWindowProc,hWnd,ourMSG,wParam,lParam
+        ret
+    .ENDIF
+	
+	ExitCode:
+    xor eax, eax
+    ret
+WndMainProc endp
+
+WndWarnProc proc hWnd:HWND, ourMSG:UINT, wParam:WPARAM, lParam:LPARAM
+	; on window close
+	.IF ourMSG==WM_CLOSE
+		; exit program
+		invoke DestroyWindow,hWnd
+        invoke PostQuitMessage,NULL 
+
+    .ELSEIF ourMSG==WM_CREATE
+		invoke CreateWindowEx,NULL,
+                offset NameOfTheButton, offset TextForOKButton,
+                WS_CHILD or WS_VISIBLE or BS_CENTER or BS_TEXT or BS_VCENTER,
+                65, 65, 150, 30,
+                hWnd, 7003, hInstance, NULL
+				
+		invoke CreateWindowEx,NULL,
+                offset NameOfTheText, offset StartingText,
+                WS_VISIBLE or WS_CHILD or BS_TEXT or SS_CENTER  or BS_VCENTER,
+                16, 10, 250, 50,
+                hWnd, 7004, hInstance, NULL
+				
+	.ELSEIF ourMSG==WM_COMMAND
+		; exit program
+		invoke DestroyWindow,hWnd
+        invoke PostQuitMessage,NULL 
+	  
+    .ELSE
+		 ; process the message
+        invoke DefWindowProc,hWnd,ourMSG,wParam,lParam
+        ret
+    .ENDIF
+	 
+	ExitCode:
+    xor    eax,eax
+    ret
+WndWarnProc endp
+
+end start
