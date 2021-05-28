e26949d9def2bfa75e1b90dfa6b40ab3101d464e
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-05-28 21:29:39 +0300

    ✨: finish [.bat]'s

--- Labs/Lab8/Code/Dynamic-WithoutEntry/8-9-IP93-Dominskyi-Dynamic-WithoutEntry.bat
+++ Labs/Lab8/Code/Dynamic-WithoutEntry/8-9-IP93-Dominskyi-Dynamic-WithoutEntry.bat
@@ -10,11 +10,11 @@ set NameOfTheLibraryAsOBJParametr="8-9-IP93-Dominskyi-Dynamic-WithoutEntry-Libra
 :: We can write there, for example, %OurDisk%\masm32\bin\ml, but We have masm commands in environment variables, so need to write only relative path
 :: There We are combining main file with dll one .exe
 \masm32\bin\ml /c /coff "%NameOfTheLibraryAsASMParametr%"
-\masm32\bin\Link.exe /OUT:"%NameOfTheLibraryAsDLLParametr%" /EXPORT:DoArithmeticOperations /NOENTRY /DLL "%NameOfTheLibraryAsOBJParametr%"
+\masm32\bin\Link.exe /OUT:"%NameOfTheLibraryAsDLLParametr%" /DLL /EXPORT:DoArithmeticOperations /NOENTRY "%NameOfTheLibraryAsOBJParametr%"
 
 \masm32\bin\ml /c /coff "%NameOfTheFileAsASMParametr%"
 
-\masm32\bin\Link.exe /SUBSYSTEM:console "%NameOfTheFileAsOBJParametr%"
+\masm32\bin\Link.exe /subsystem:console "%NameOfTheFileAsOBJParametr%"
 
 :: if You want window to pause after procedure, then uncomment next row (but You need it, only if You have some problem with code)
 :: pause
--- Labs/Lab8/Code/Static-WithoutEntry/8-9-IP93-Dominskyi-Static-WithoutEntry.bat
+++ Labs/Lab8/Code/Static-WithoutEntry/8-9-IP93-Dominskyi-Static-WithoutEntry.bat
@@ -11,11 +11,11 @@ set NameOfTheLibraryAsOBJParametr="8-9-IP93-Dominskyi-Static-WithoutEntry-Librar
 :: There We are combining main file with dll one .exe
 :: We can write there, for example, %OurDisk%\masm32\bin\ml, but We have masm commands in environment variables, so need to write only relative path
 \masm32\bin\ml /c /coff "%NameOfTheLibraryAsASMParametr%"
-\masm32\bin\Link.exe /OUT:"%NameOfTheLibraryAsDLLParametr%" /DEF:%NameOfTheFileAsDEFParametr% /NOENTRY /DLL "%NameOfTheLibraryAsOBJParametr%"
+\masm32\bin\Link.exe /OUT:"%NameOfTheLibraryAsDLLParametr%" /DLL /NOENTRY /DEF:%NameOfTheFileAsDEFParametr% "%NameOfTheLibraryAsOBJParametr%"
 
 \masm32\bin\ml /c /coff "%NameOfTheFileAsASMParametr%"
 
-\masm32\bin\Link.exe /SUBSYSTEM:console "%NameOfTheFileAsOBJParametr%"
+\masm32\bin\Link.exe /subsystem:console "%NameOfTheFileAsOBJParametr%"
 
 :: if You want window to pause after procedure, then uncomment next row (but You need it, only if You have some problem with code)
 :: pause
